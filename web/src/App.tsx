3de5c2c737ea6b2c858a051129b8a125ace6a8a7
Ryan Williams <r.d.williams.ca@gmail.com>
2022-07-12 14:47:52 -0700

    added cors option to allow requests from localhost:3000

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,29 @@
-import { useEffect } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import {Repo} from '../../api/src/models/Repo'
 import './App.css';
 
 export function App() {
 
-  useEffect(() => {
+  const [data, setData] = useState<any>(null)
 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = await axios.get('http://localhost:4000/repos')
+      setData(fetchedData)
+    }
+    
+    fetchData()
+      .catch(err => {
+        console.log(err)
+      })
   }, [])
   
 
   return (
     <div className="App">
+      <p>{data}</p>
       
     </div>
   );
